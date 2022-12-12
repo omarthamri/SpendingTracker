f0b43dbd57d7a3a69b5b172c0714d0e95086f94a
Omar Thamri <omarthamri4@gmail.com>
2022-12-12 21:17:16 +0100

    Initial Commit

---
+++ SpendingTracker/SpendingTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  SpendingTrackerApp.swift
+//  SpendingTracker
+//
+//  Created by omar thamri on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpendingTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
